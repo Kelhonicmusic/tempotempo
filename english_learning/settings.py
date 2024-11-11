f9427bbba9838ab07dd6cfd5eff53246a69cc157
Kelhonicmusic <89862866+Kelhonicmusic@users.noreply.github.com>
2024-11-11 23:39:03 +0300

    Update settings.py

--- english_learning/settings.py
+++ english_learning/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k7rjtg_j--vc4u25stm3qaejl)mi^pu1j6&t6hx+7ce4+*&ojg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['projo-f2gm.onrender.com']
 
 PAYPAL_CLIENT_ID = 'YOUR_PAYPAL_CLIENT_ID'
 PAYPAL_CLIENT_SECRET = 'YOUR_PAYPAL_CLIENT_SECRET'
